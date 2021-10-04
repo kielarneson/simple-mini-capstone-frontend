e15e36731b01c1bca7be1e9aa81e09db4954373b
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-04 11:04:53 -0500

    Added createProduct function with post request to add a new product to our backend and display it with our frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <h1>Create New Product</h1>
+    <button v-on:click="createProduct()">Create</button>
     <h1>All Products</h1>
     <div v-for="product in products" v-bind:key="product.id">
       <h2>{{ product.name }}</h2>
@@ -28,6 +30,18 @@ export default {
       axios.get("http://localhost:3000/products").then((response) => {
         this.products = response.data;
         console.log("All products", this.products);
+      });
+    },
+    createProduct: function () {
+      var params = {
+        name: "Test name",
+        description: "Test description",
+        price: 1,
+        image_url: "Test image.url",
+      };
+      axios.post("http://localhost:3000/products", params).then((response) => {
+        console.log("Success", response.data);
+        this.products.push(response.data);
       });
     },
   },
